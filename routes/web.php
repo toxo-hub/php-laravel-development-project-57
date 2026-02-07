29916b1fee4c702bce2b0c1ae93744784cc2422f
toxa <azelydkov@gmail.com>
2026-02-07 03:34:09 +0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use App\Http\Controllers\{ArticleController, PageController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
-
-Route::resource('articles', ArticleController::class);
-
-Route::get('/about', [PageController::class, 'about'])->name('about');
