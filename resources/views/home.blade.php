f743c007931681c64ecaad76d8eb60e5beda0fd4
toxa <azelydkov@gmail.com>
2026-02-07 03:31:43 +0300

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,10 @@
 @section('title', 'Главная')
 
 @section('content')
-    <div>
-        <h1>Главная</h1>
-        <br>
-        <hr>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem doloremque eaque eum excepturi laboriosam modi nobis quibusdam repellat sit, suscipit tempore voluptatem! Animi asperiores blanditiis, consequatur corporis cupiditate eligendi est in libero nemo omnis qui quod sint tenetur. Aliquam aperiam deleniti distinctio doloribus eius exercitationem modi, molestiae nobis perferendis recusandae repellat similique, voluptas? Ab, aspernatur atque blanditiis cupiditate deserunt, dolorum eligendi esse explicabo fuga inventore ipsa modi odio possimus quod sunt voluptas voluptatum? Delectus distinctio doloribus ducimus, esse et facere necessitatibus nulla quibusdam quis quod quos saepe suscipit temporibus! Ad, blanditiis dolorem ex exercitationem fugit illum libero reiciendis ut.</p>
+    <div class="row">
+        <div class="col-12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cumque cupiditate ea eum expedita iusto libero
+            natus nisi nostrum, odit pariatur placeat repellendus saepe sapiente sed sunt totam veritatis voluptates!
+        </div>
     </div>
 @endsection
