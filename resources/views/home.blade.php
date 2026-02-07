1ded51503905a5eedaf455c76c164671872c3354
toxa <azelydkov@gmail.com>
2026-02-07 03:26:25 +0300

    Init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Главная')
+
+@section('content')
+    <div>
+        <h1>Главная</h1>
+        <br>
+        <hr>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem doloremque eaque eum excepturi laboriosam modi nobis quibusdam repellat sit, suscipit tempore voluptatem! Animi asperiores blanditiis, consequatur corporis cupiditate eligendi est in libero nemo omnis qui quod sint tenetur. Aliquam aperiam deleniti distinctio doloribus eius exercitationem modi, molestiae nobis perferendis recusandae repellat similique, voluptas? Ab, aspernatur atque blanditiis cupiditate deserunt, dolorum eligendi esse explicabo fuga inventore ipsa modi odio possimus quod sunt voluptas voluptatum? Delectus distinctio doloribus ducimus, esse et facere necessitatibus nulla quibusdam quis quod quos saepe suscipit temporibus! Ad, blanditiis dolorem ex exercitationem fugit illum libero reiciendis ut.</p>
+    </div>
+@endsection
